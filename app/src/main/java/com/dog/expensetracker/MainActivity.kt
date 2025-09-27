d3b61035b7c0b7f35c06ec0bf67622f679cee3e5
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-09-27 23:01:59 +0300

    Cleaned old implementation

--- app/src/main/java/com/dog/expensetracker/MainActivity.kt
+++ app/src/main/java/com/dog/expensetracker/MainActivity.kt
@@ -1,43 +1,19 @@
 package com.dog.expensetracker
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.dog.expensetracker.data.local.ExpenseDatabase
-import com.dog.expensetracker.data.repository.ExpenseRepository
-import com.dog.expensetracker.navigation.NavGraph
+import com.dog.expensetracker.navigation.RootNavGraph
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ExpenseTrackerNavHost()
+            RootNavGraph() // use your new navigation setup
         }
     }
 }
 
-@Composable
-
-fun ExpenseTrackerNavHost() {
-    val navController = rememberNavController()
-
-    val context = LocalContext.current
-
-    val db = ExpenseDatabase.getDatabase(context)
-
-    val repository = ExpenseRepository(db.expenseDao())
-
-    NavGraph(navController = navController)
-
-}
 
