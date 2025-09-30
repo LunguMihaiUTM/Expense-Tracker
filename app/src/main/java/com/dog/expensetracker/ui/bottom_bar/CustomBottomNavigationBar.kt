6565f5df8bb7ade9355003a6c65670d0c4caa42e
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-09-30 13:21:07 +0300

    Made the bottom bar for global use

--- app/src/main/java/com/dog/expensetracker/ui/bottom_bar/CustomBottomNavigationBar.kt
+++ app/src/main/java/com/dog/expensetracker/ui/bottom_bar/CustomBottomNavigationBar.kt
@@ -1,22 +1,20 @@
-package com.dog.expensetracker.ui.common
+package com.dog.expensetracker.ui.bottom_bar
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBalanceWallet
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.BarChart
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,126 +24,113 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.dog.expensetracker.features.home.HomeContract
-import com.dog.expensetracker.navigation.LocalRootNavigator
-import com.dog.expensetracker.navigation.RootNavDestinations
+import com.dog.expensetracker.ui.common.AddTransactionDialog
+import com.dog.expensetracker.ui.global.GlobalContract
 
 
 @Composable
 fun CustomBottomNavigationBar(
-    selectedTab: Int = 0,
-    onTabSelected: (Int) -> Unit = {},
-    onEvent: (HomeContract.Event) -> Unit
+    modifier: Modifier,
+    selectedTab: BottomNavTab,
+    onTabSelected: (BottomNavTab) -> Unit = {},
+    onEvent: (GlobalContract.Event) -> Unit
 ) {
-    val navController = LocalRootNavigator.current
-    Box(
-        modifier = Modifier.fillMaxWidth()
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        // Bottom Navigation Bar
-        NavigationBar(
-            modifier = Modifier.fillMaxWidth(),
-            containerColor = Color.White,
-            tonalElevation = 8.dp
-        ) {
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.Home,
-                        contentDescription = "Home",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedTab == 0,
-                onClick = { navController.navigate(RootNavDestinations.Home) },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color(0xFF6C5CE7),
-                    unselectedIconColor = Color.Gray,
-                    indicatorColor = Color.Transparent
-                )
-            )
+        BottomNavigationBarItem(
+            modifier = Modifier.weight(1f),
+            item = BottomNavTab.HOME,
+            isSelected = selectedTab == BottomNavTab.HOME,
+            onSelect = { onTabSelected(BottomNavTab.HOME) }
+        )
 
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.BarChart,
-                        contentDescription = "Stats",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedTab == 1,
-                onClick = { navController.navigate(RootNavDestinations.Overview)},
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color(0xFF6C5CE7),
-                    unselectedIconColor = Color.Gray,
-                    indicatorColor = Color.Transparent
-                )
-            )
-
-            // Empty space for FAB
-            NavigationBarItem(
-                icon = { Spacer(modifier = Modifier.size(24.dp)) },
-                selected = false,
-                onClick = { },
-                enabled = false
-            )
-
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.AccountBalanceWallet,
-                        contentDescription = "Transactions",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedTab == 3,
-                onClick = { onTabSelected(3) },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color(0xFF6C5CE7),
-                    unselectedIconColor = Color.Gray,
-                    indicatorColor = Color.Transparent
-                )
-            )
-
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.AccountCircle,
-                        contentDescription = "Profile",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedTab == 4,
-                onClick = { onTabSelected(4) },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color(0xFF6C5CE7),
-                    unselectedIconColor = Color.Gray,
-                    indicatorColor = Color.Transparent
-                )
-            )
-        }
+        BottomNavigationBarItem(
+            modifier = Modifier.weight(1f),
+            item = BottomNavTab.OVERVIEW,
+            isSelected = selectedTab == BottomNavTab.OVERVIEW,
+            onSelect = { onTabSelected(BottomNavTab.OVERVIEW) }
+        )
 
         var showDialog by remember { mutableStateOf(false) }
 
-        FloatingActionButton(
-            onClick = { showDialog = true },
-            modifier = Modifier
-                .align(Alignment.Center)
-                .offset(y = (-12).dp),
-            containerColor = Color(0xFF6C5CE7),
-            contentColor = Color.White,
-            elevation = FloatingActionButtonDefaults.elevation(8.dp)
+
+        Box(
+            Modifier.weight(1f),
+            contentAlignment = Alignment.Center
         ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Transaction")
+            FloatingActionButton(
+                onClick = { showDialog = true },
+                modifier = Modifier
+                    .offset(y = (-12).dp),
+                containerColor = Color(0xFF6C5CE7),
+                contentColor = Color.White,
+                elevation = FloatingActionButtonDefaults.elevation(8.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Transaction")
+            }
         }
+
+
+        BottomNavigationBarItem(
+            modifier = Modifier.weight(1f),
+            item = BottomNavTab.HOME,
+            isSelected = selectedTab == BottomNavTab.HOME,
+            onSelect = { onTabSelected(BottomNavTab.HOME) }
+        )
+
+        BottomNavigationBarItem(
+            modifier = Modifier.weight(1f),
+            item = BottomNavTab.HOME,
+            isSelected = selectedTab == BottomNavTab.HOME,
+            onSelect = { onTabSelected(BottomNavTab.HOME) }
+        )
+
 
         if (showDialog) {
             AddTransactionDialog(
                 onDismiss = { showDialog = false },
                 onSave = { expense ->
-                    onEvent(HomeContract.Event.AddExpense(expense))  // <-- go through event system
+                    onEvent(GlobalContract.Event.AddExpense(expense))
                     showDialog = false
                 }
             )
         }
     }
+}
+
+
+@Composable
+private fun BottomNavigationBarItem(
+    modifier: Modifier,
+    item: BottomNavTab,
+    isSelected: Boolean,
+    onSelect: () -> Unit
+) {
+    val iconResId = when (item) {
+        BottomNavTab.HOME -> Icons.Default.Home
+        BottomNavTab.OVERVIEW -> Icons.Filled.BarChart
+    }
+
+    val color = if (isSelected) Color(0xFF6C5CE7) else Color.Gray
+
+
+
+    Row(
+        modifier = modifier
+            .clickable { onSelect() }
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.Center,
+
+        ) {
+        Icon(
+            imageVector = iconResId,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = color
+        )
+    }
+
+
 }
